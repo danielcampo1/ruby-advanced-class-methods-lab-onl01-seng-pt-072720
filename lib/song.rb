d74380b2b0c41d79dd8067dd2e93723c681b3cf2
daniel campo <campo_daniel94@yahoo.com>
2020-08-18 23:30:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,51 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = self.new
+    song.save
+    song
+end
 
+  def self.new_by_name(name)
+  song = self.new
+  song.name = name
+  song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+  
+  
+  def self.find_by_name(name)
+    @@all.detect do |song|
+      song.name == name 
+    end
+end  
+  
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+    # or
+    # self.find_by_name(name) || self.create_by_name(name)
+end
+
+  def self.alphabetical
+    @@all.sort do |a,b|
+      a.name <=> b.name 
+      #or
+      # @@all.sort_by{|song| song.name }
+    end
+  end
+  
+  def self.new_from_filename(filename)
+end
 end
