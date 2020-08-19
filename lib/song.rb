80a53eda1b75f2b4df386a00a167bea2e2d1700b
daniel campo <campo_daniel94@yahoo.com>
2020-08-19 00:10:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -57,7 +57,7 @@ end
   end
   
   def self.new_from_filename(filename)
-    split_filename = filename.chop.chop.chop.chop.split(" - ")
+    split_filename = filename.chomp(".mp3").split(" - ")
     song = Song.new
     song.name = split_filename[1]
     song.artist_name = split_filename[0]
